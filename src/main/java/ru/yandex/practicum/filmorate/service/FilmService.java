03cf824911fd8e1073739b117ef28e0ce346cc7c
unknown <SVyakimchuk@yandex.ru>
2023-05-08 23:19:03 +0400

    Доработка по checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -11,11 +11,18 @@ import java.util.List;
 public interface FilmService {
 
     Collection<Film> getAllFilms();
+
     Film findFilmById(int id);
+
     Film addFilm(Film film);
+
     void removeFilm(int id);
+
     Film updateFilm(Film updateFilm);
+
     Film addLike(int id, int userId);
+
     Film removeLike(int id, int userId);
+
     List<Film> findPopularFilms(Integer count);
 }
